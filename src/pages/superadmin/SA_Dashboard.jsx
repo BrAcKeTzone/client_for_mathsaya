53e0b31994354887c0590fa9e666d11fdef7eedb
JhulPona <jpskiemaniwang@gmail.com>
2023-12-30 17:41:31 +0800

    made the dashboard be responsive

--- src/pages/superadmin/SA_Dashboard.jsx
+++ src/pages/superadmin/SA_Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { RiMailFill, RiAdminFill } from "react-icons/ri";
@@ -15,15 +15,32 @@ function SA_Dashboard() {
   const [totalSuperAdmins, setTotalSuperAdmins] = useState(0);
 
   useEffect(() => {
-    const superadmin = Cookies.get("spr");
+    const superadmin = JSON.parse(Cookies.get("spr"));
     if (!superadmin) {
       Navigate("/super-login");
-    } else {
-      setIsLoading(false);
+      return;
     }
+
+    console.log(superadmin.id);
+
+    // Check if the superadmin.id exists in Super Admin entries
+    const checkSuperAdminId = async () => {
+      try {
+        const response = await axios.post(
+          `${server_url}/superadmin/check-superadmin/${superadmin.id}`
+        );
+
+        console.log(response.data.message);
+        fetchData();
+      } catch (error) {
+        Cookies.remove("spr");
+        Navigate("/super-login");
+      }
+    };
 
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const responseEmails = await axios.get(
           `${server_url}/cust-support/total`
         );
@@ -40,38 +57,52 @@ function SA_Dashboard() {
         setTotalSuperAdmins(responseSuperAdmins.data.totalSuperAdmins);
       } catch (error) {
         console.error("Error fetching data:", error);
-      }
-      finally {
+      } finally {
         setIsLoading(false);
       }
     };
 
-    fetchData();
+    checkSuperAdminId();
   }, [Navigate]);
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
-        {isLoading ? (
+      {isLoading ? (
         <span className="loader"></span>
       ) : (
-      <div className="grid grid-cols-3 gap-4">
-        <div className=" flex justify-center flex-col items-center bg-blue-300 p-4 rounded shadow-md hover:bg-blue-400 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
-          <RiMailFill className="text-8xl mb-2" />
-          <p className="text-xl font-bold">{totalEmails}</p>
-          <p>Total Emails</p>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="bg-blue-300 p-4 rounded shadow-md hover:bg-blue-400 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+            <Link
+              to="/super-emails"
+              className="flex justify-center flex-col items-center"
+            >
+              <RiMailFill className="text-8xl mb-2" />
+              <p className="text-xl font-bold">{totalEmails}</p>
+              <p>Total Emails</p>
+            </Link>
+          </div>
+          <div className="bg-green-300 p-4 rounded shadow-md hover:bg-green-400 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+            <Link
+              to="/super-teachs"
+              className="flex justify-center flex-col items-center"
+            >
+              <FaUserGraduate className="text-8xl mb-2" />
+              <p className="text-xl font-bold">{totalTeachers}</p>
+              <p>Total Teachers</p>
+            </Link>
+          </div>
+          <div className="bg-purple-300 p-4 rounded shadow-md hover:bg-purple-400 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+            <Link
+              to="/super-supers"
+              className="flex justify-center flex-col items-center"
+            >
+              <RiAdminFill className="text-8xl mb-2" />
+              <p className="text-xl font-bold">{totalSuperAdmins}</p>
+              <p>Total Super Admins</p>
+            </Link>
+          </div>
         </div>
-        <div className=" flex justify-center flex-col items-center bg-green-300 p-4 rounded shadow-md hover:bg-green-400 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
-          <FaUserGraduate className="text-8xl mb-2" />
-          <p className="text-xl font-bold">{totalTeachers}</p>
-          <p>Total Teachers</p>
-        </div>
-        <div className=" flex justify-center flex-col items-center bg-purple-300 p-4 rounded shadow-md hover:bg-purple-400 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
-          <RiAdminFill className="text-8xl mb-2" />
-          <p className="text-xl font-bold">{totalSuperAdmins}</p>
-          <p>Total Super Admins</p>
-        </div>
-      </div>
-    )}
+      )}
     </div>
   );
 }
