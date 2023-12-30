d2fd29e33852fee70f66a3472b0b1a874fe8956d
JhulPona <jpskiemaniwang@gmail.com>
2023-12-30 21:31:49 +0800

    designed the front end for displaying super admin list

--- src/pages/superadmin/SA_SuperAdminList.jsx
+++ src/pages/superadmin/SA_SuperAdminList.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { RiAdminFill } from "react-icons/ri";
+import { FaRegPlusSquare, FaRegEdit, FaTrashAlt } from "react-icons/fa";
+import "../../assets/styles/hideVerticalScrollbar.css";
+
+const server_url = import.meta.env.VITE_SERVER_LINK;
 
 function SA_SuperAdminList() {
-  return <div>SA_SuperAdminList</div>;
+  const Navigate = useNavigate();
+  const [superAdmins, setSuperAdmins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let superadmin = Cookies.get("spr");
+    if (!superadmin) {
+      Navigate("/super-login");
+      return;
+    }
+
+    superadmin = JSON.parse(Cookies.get("spr"));
+
+    const checkSuperAdminId = async () => {
+      try {
+        const response = await axios.post(
+          `${server_url}/superadmin/check-superadmin/${superadmin.id}`
+        );
+
+        console.log(response.data.message);
+        fetchSuperAdmins();
+      } catch (error) {
+        Cookies.remove("spr");
+        Navigate("/super-login");
+      }
+    };
+
+    const fetchSuperAdmins = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          `${server_url}/superadmin/superadmins`
+        );
+        setSuperAdmins(response.data);
+      } catch (error) {
+        console.error("Error fetching super admins:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkSuperAdminId();
+  }, [Navigate]);
+
+  return (
+    <div className="flex flex-col items-center h-screen bg-emerald-600">
+      <h1 className="text-white text-4xl mb-4 p-4">Super Admin List</h1>
+      <div className="overflow-y-auto overflow-x-hidden max-h-[80vh]">
+        {isLoading ? (
+          <span className="loader"></span>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-2">
+            {superAdmins.map((superAdmin) => (
+              <div
+                key={superAdmin.id}
+                className={`bg-green-500 hover:bg-green-400 flex justify-center flex-col items-center p-4 rounded shadow-md transition duration-300 ease-in-out transform hover:scale-105`}
+              >
+                <RiAdminFill className="text-8xl mb-2" />
+                <table className="flex justify-center flex-col items-center ">
+                  <th>
+                    {superAdmin.firstname} {superAdmin.lastname}
+                  </th>
+                  <tr />
+                  <td>{superAdmin.email}</td>
+                  <tr />
+                  <td>{superAdmin.gender}</td>
+                  <tr />
+                  <td className="grid grid-cols-3 gap-4 p-2 border-t-2">
+                    <button className="bg-blue-500 hover:bg-blue-400 p-2 rounded">
+                      <FaRegPlusSquare className="text-2xl" />
+                    </button>
+                    <button className="bg-yellow-500 hover:bg-yellow-400 p-2 rounded">
+                      <FaRegEdit className="text-2xl" />
+                    </button>
+                    <button className="bg-red-500 hover:bg-red-400 p-2 rounded">
+                      <FaTrashAlt className="text-2xl" />
+                    </button>
+                  </td>
+                </table>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default SA_SuperAdminList;
