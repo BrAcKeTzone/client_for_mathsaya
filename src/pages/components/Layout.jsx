8639f3984ab9364f6c685b4fa7c5bcab81c9c57f
JhulPona <jpskiemaniwang@gmail.com>
2024-02-02 01:04:45 +0800

    added bottombar in the layout

--- src/pages/components/Layout.jsx
+++ src/pages/components/Layout.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Bottombar from "./Bottombar";
+import Cookies from "js-cookie";
 
-function NavLayout({ children, userRole }) {
+function NavLayout({ children, userRole, server_url, usr }) {
+  // console.log(userRole);
+  usr = JSON.parse(Cookies.get("SESSION_ID"));
+
   return (
     <>
-      <Navbar userRole={userRole} />
+      <Navbar userRole={userRole} server_url={server_url} usr={usr.id} />
       <div className="content-container">{children}</div>
+      <Bottombar server_url={server_url} usr={usr.id} />
     </>
   );
 }
